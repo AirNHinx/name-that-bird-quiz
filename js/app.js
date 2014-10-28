ba45db1f77b49b08a5530fa8de590c8795cd68c6
Erin Hinkle <erinhinx5@gmail.com>
2014-10-28 12:01:42 -0400

    fdsfsd

--- js/app.js
+++ js/app.js
@@ -29,15 +29,39 @@ $(document).ready(function(){
 		q: "Name that bird!",
 		options: ["Adelie Penguin", "Japanese Quail", "Puffin", "Rockhopper Penguin"],
 		qIndex: 4,
-		correct: 1,
+		correct: 3,
 		response: "Blah blah blah Puffin blah blah."					
-	}, {
+		}, {
 		q: "Name that bird!",
 		options: ["Scarlet Tanager", "Purple Finch", "Female Cardinal", "House Finch"],
 		qIndex: 5,
 		correct: 2,
 		response: "Blah blah blah Purple Finch blah blah."			
-	}]
+		}, {
+		q: "Name that bird!",
+		options: ["Mallard Duck", "Woodduck", "Black East Indie Duck", "Appleyard Duck"],
+		qIndex: 6,
+		correct: 2,
+		response: "Blah blah blah Woodduck blah blah."		
+		}, {
+		q: "Name that bird!",
+		options: ["Blue Jay", "North American Bluebird", "Western Scrub Jay", "Stellar's Jay"],
+		qIndex: 7,
+		correct: 4,
+		response: "Blah blah blah Stellar's Jay blah blah."			
+		}, {
+		q: "Name that bird!",
+		options: ["Barn Owl", "Barred Owl", "Eagle Owl", "Northern Hawk Owl"],
+		qIndex: 8,
+		correct: 3,
+		response: "Blah blah blah Eagle Owl blah blah."				
+		}, {
+		q: "Name that bird!",
+		options: ["Raibow Bee Eater", "bird option", "another bird option", "Greater Bird of Paradise"],
+		qIndex: 9,
+		correct: 4,
+		response: "Blah blah blah Greater Bird of Paradise blah blah."			
+		}]
 
 	/*--- initiate first question---*/
 	$('.initiate').click(function() {
