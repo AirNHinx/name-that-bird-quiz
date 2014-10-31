4ca75be6d2f364718f269c9297d429a659e48d41
Erin Hinkle <erinhinx5@gmail.com>
2014-10-31 11:26:09 -0400

    bird

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ var currentBird = 0;
 		$('.bird').html('<img class="bird-zero" src="img/bird/black-capped-chickadee.jpg">');
 		$('#bird-options').addClass("option-container");
 		$("#bird-zero").show();
-		$("#bord-zero>.option-two").addClass("correct-bird");
+		$("#bird-zero>.option-two").addClass("correct-bird");
 	})
 
 	/*---indicate selected answer---*/
