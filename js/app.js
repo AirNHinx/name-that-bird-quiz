d20bca722bd3ef3d4a896dad96786020415f1dea
Erin Hinkle <erinhinx5@gmail.com>
2014-10-31 15:48:32 -0400

    hmm

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var birdImg = ['black-capped-chickadee.jpg', 'snowy-egret.jpg', 'raven.jpg', 'vi
 		$(".mid-msg").html("<p>Name that bird!</p>");
 		$(".btm-msg").hide();
 		//$(".bird").html("<img class='birdImg' + currentBird + "' src='img/bird/black-capped-chickadee.jpg'>");
-		$(".bird-img").attr("src", "img/bird/" + birdImg[currentBird]);
+		$(".bird-img").html('<img' + .attr("src", "img/bird/" + birdImg[currentBird]); + '>');
 		$("#bird-options").addClass("option-container");
 		$("#bird-" + currentBird).show();
 		//$("#bird-zero").show();
