60ee76ddfffb2d913459139cb5556fd980e4c58c
Erin Hinkle <erinhinx5@gmail.com>
2014-11-02 18:27:11 -0500

    ok

--- js/app.js
+++ js/app.js
@@ -25,8 +25,8 @@ var birdImg = ['black-capped-chickadee.jpg', 'snowy-egret.jpg', 'raven.jpg', 'vi
 		$("#bird-options").addClass("option-container");
 		console.log(correctBird[currentBird]);
 		$("#bird" + currentBird).show();
-		//$('#bird' + currentBird + '>ul>li.option').addClass('wrong-bird');
-		$("#bird0>ul>.option2").addClass("correct-bird");
+		$("#bird" + currentBird + ">ul>li.option").addClass("correct-bird");
+		//$("#bird0>ul>.option2").addClass("correct-bird");
 		//$('#bird' + currentBird + '>ul>li.option' + correctBird[currentBird]).addClass('correct-bird')/*.removeClass('wrong-bird')*/;
 		$(".initiate").hide();
 		currentBird += 1;
