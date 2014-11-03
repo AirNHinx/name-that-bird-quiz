50e74c42eb0d2215b7500720969338b0173416c2
Erin Hinkle <erinhinx5@gmail.com>
2014-11-02 20:57:36 -0500

    ?

--- js/app.js
+++ js/app.js
@@ -55,7 +55,8 @@ var birdImg = ['black-capped-chickadee.jpg', 'snowy-egret.jpg', 'raven.jpg', 'vi
 			$(".answer-correct").hide();
 		} else if (t.hasClass("correct-bird")){
 			console.log("correct-bird");
-			$(".answer-correct").addClass("correct-bird-confirm").show();
+			$(".correct-bird").addClass("correct-bird-confirm");
+			$(".answer-correct").show();
 			$(".answer-wrong").hide();
 			$(".wrong-bird").hide();
 			$(".mid-msg").hide();
